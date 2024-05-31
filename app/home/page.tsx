d2749daa4f04069ad8b2dca53f989eead829c9b4
zeyuanzhao <14340342+zeyuanzhao@users.noreply.github.com>
2024-05-31 15:00:30 -0400

    delete home prop

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -6,7 +6,7 @@ import Sidebar from '../components/Sidebar';
 import VideoPage from '../components/VideoPage';
 import ArticlePage from '../components/ArticlePage';
 
-export default function Home({title}:{title: string}) {
+export default function Home() {
   const videoTitles = [{title: "How a Bill Becomes a Law", url: "how-a-bill-becomes-a-law", link: "https://www.youtube.com/embed/tgbNymZ7vqY"}, {title: "Rah Rah Ooh La La", url: "how-to-get-mr-c-on-the-scotus", link: "https://www.youtube.com/embed/tgbNymZ7vqY"}, {title: "Nom Nom Nom Nom", url: "the-world-of-campaign-finance", link: "https://www.youtube.com/embed/tgbNymZ7vqY"}];
 
   return (
