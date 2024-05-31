6b742e1c0e28417b90667f82fbcf57eec2c26d31
zeyuanzhao <14340342+zeyuanzhao@users.noreply.github.com>
2024-05-31 14:43:17 -0400

    import fix

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -5,10 +5,6 @@ import Header from '../components/Header';
 import Sidebar from '../components/Sidebar';
 import VideoPage from '../components/VideoPage';
 import ArticlePage from '../components/ArticlePage';
-import { 
-  BrowserRouter as Router, Routes, 
-  Route, Link
-} from "react-router-dom"; 
 
 export default function Home({title}:{title: string}) {
   const videoTitles = [{title: "How a Bill Becomes a Law", url: "how-a-bill-becomes-a-law", link: "https://www.youtube.com/embed/tgbNymZ7vqY"}, {title: "Rah Rah Ooh La La", url: "how-to-get-mr-c-on-the-scotus", link: "https://www.youtube.com/embed/tgbNymZ7vqY"}, {title: "Nom Nom Nom Nom", url: "the-world-of-campaign-finance", link: "https://www.youtube.com/embed/tgbNymZ7vqY"}];
