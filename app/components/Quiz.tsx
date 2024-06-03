9b20ffd2c786a9cc4f0e38e05021fdb7b8e414c1
zeyuanzhao <14340342+zeyuanzhao@users.noreply.github.com>
2024-06-02 21:48:00 -0400

    home page text and button

--- app/components/Quiz.tsx
+++ app/components/Quiz.tsx
@@ -3,7 +3,7 @@ import ProgressBar from "./ProgressBar";
 
 export default function Quiz() {
     return (
-        <div className="w-3/4 h-full min-h-[100vh] pb-16 flex flex-col items-center">
+        <div className="w-3/4 min-h-[100vh] pb-16 flex flex-col items-center h-[calc(100vh-64px)] overflow-scroll px-8">
             <h1 className="font-semibold text-3xl mt-6">How a Bill Becomes a Law</h1>
             <h2 className="font-normal text-xl mt-2">By John Doe</h2>
 
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,19 @@
+import { Button } from "@nextui-org/react";
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <div>
-      <h1>Home</h1>
+    <div className="h-[calc(100vh-64px)] overflow-scroll px-8">
+      <div className="text-center flex flex-col justify-center items-center h-full">
+        <div>
+          <h1 className="text-3xl mb-4">Welcome to NSL Forever</h1>
+          <h2 className="mb-8">
+            Access AP NSL review modules containing videos, articles, and
+            quizzes.
+          </h2>
+          <Link href="/learn/0/video"><Button className="bg-[#0E793C] text-white">Get Started</Button></Link>
+        </div>
+      </div>
     </div>
   );
 }
