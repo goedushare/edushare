cbe41fd5973fbb9cc6e705ba37f3ebb428c6e666
zeyuanzhao <14340342+zeyuanzhao@users.noreply.github.com>
2024-06-02 21:32:55 -0400

    navbar links

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -12,24 +12,29 @@ export default function Header() {
   return (
     <Navbar position="sticky">
       <NavbarBrand>
-        <p className="font-bold text-inherit">NSL Forever!</p>
+        <Link href="/" className="text-black">
+          <p className="font-bold text-inherit">NSL Forever!</p>
+        </Link>
       </NavbarBrand>
       <NavbarContent className="hidden sm:flex gap-4" justify="center">
         <NavbarItem>
-          <Link color="foreground" href="#">
+          <Link color="foreground" href="/learn/0/article">
             Modules
           </Link>
         </NavbarItem>
       </NavbarContent>
       <NavbarContent justify="end">
         <NavbarItem>
-          <Button as={Link} className="bg-[#0E793C] text-white" href="#" variant="flat">
+          <Button
+            as={Link}
+            className="bg-[#0E793C] text-white"
+            href="#"
+            variant="flat"
+          >
             Upload
           </Button>
         </NavbarItem>
-        <NavbarItem>
-          123456
-        </NavbarItem>
+        <NavbarItem>123456</NavbarItem>
       </NavbarContent>
     </Navbar>
   );
