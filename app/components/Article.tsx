68f155c505e4a81ff6d1b9a0ede866ac4079b33c
elianabean <ewanginspiration@gmail.com>
2024-06-04 14:53:44 -0400

    fixed progress bar

--- app/components/Article.tsx
+++ app/components/Article.tsx
@@ -1,5 +1,7 @@
+"use client";
+import { useRef } from 'react';
+import {motion, useScroll} from "framer-motion";
 import React from "react";
-import ProgressBar from "./ProgressBar";
 import ReactMarkdown from "react-markdown";
 
 export default function Article({
@@ -11,12 +13,17 @@ export default function Article({
   authors: string;
   body: string;
 }) {
+
+  const containerRef = useRef(null)
+  const {scrollYProgress} = useScroll({
+      container: containerRef
+  });
   return (
-    <div className="w-3/4 pb-16 flex flex-col items-center h-[calc(100vh-64px)] overflow-scroll px-8">
+    <div className="w-3/4 pb-16 flex flex-col items-center h-[calc(100vh-64px)] overflow-y-scroll overflow-x-hidden px-8" ref={containerRef}>
       <h1 className="font-semibold text-3xl mt-6">{articleName}</h1>
       <h2 className="font-normal text-xl mt-2">By: {authors}</h2>
       <ReactMarkdown>{body}</ReactMarkdown>
-      <ProgressBar></ProgressBar>
+      <motion.div className="fixed bottom-0 left-[25vw] bg-gradient-to-r from-cyan-500 to-blue-500 origin-[0%] h-1 w-3/4 z-50" style={{scaleX: scrollYProgress}}></motion.div>
     </div>
   );
 }
