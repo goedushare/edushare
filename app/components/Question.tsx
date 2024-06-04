49139af81f497fb9270444740a5e2d94b1527f6d
zeyuanzhao <14340342+zeyuanzhao@users.noreply.github.com>
2024-06-04 09:22:26 -0400

    hydrate questions and answers

--- app/components/Question.tsx
+++ app/components/Question.tsx
@@ -1,3 +1,17 @@
-export default function Question() {
-  return <div>Question</div>;
+import { Button, Radio, RadioGroup } from "@nextui-org/react";
+import { QuestionType } from "./Quiz";
+
+export default function Question({ question }: { question: QuestionType }) {
+  return (
+    <div>
+      <p>{question["question"]}</p>
+      <div>
+        <RadioGroup>
+          {question["answers"].map((answer) => {
+            return <Radio value={answer} key={answer}>{answer}</Radio>;
+          })}
+        </RadioGroup>
+      </div>
+    </div>
+  );
 }
--- app/components/Quiz.tsx
+++ app/components/Quiz.tsx
@@ -6,14 +6,14 @@ import Score from "./Score";
 import Question from "./Question";
 
 
-type Question = {
+export type QuestionType = {
   id: number;
   question: string;
   answers: string[];
   correct: number;
 };
 
-export default function Quiz({ questions }: { questions: Question[] }) {
+export default function Quiz({ questions }: { questions: QuestionType[] }) {
   const [currQuestion, setCurrQuestion] = React.useState(0);
   const [answers, setAnswers] = React.useState([]);
   const [end, setEnd] = React.useState(false);
@@ -22,7 +22,7 @@ export default function Quiz({ questions }: { questions: Question[] }) {
     <div className="w-3/4 min-h-[100vh] pb-16 h-[calc(100vh-64px)] overflow-scroll px-8">
       <form action="">
         <h1>Question {currQuestion + 1} of {questions.length}</h1>
-          {end ? <Score /> : <Question />}
+          {end ? <Score /> : <Question question={questions[currQuestion]} />}
         <div>
           {end ? <Button>Try Again</Button> : <Button>Submit</Button>}
         </div>
