31dda4eab578298814f819987602162385bff606
zeyuanzhao <14340342+zeyuanzhao@users.noreply.github.com>
2024-06-04 21:20:57 -0400

    video and article layout adjust

--- app/components/Article.tsx
+++ app/components/Article.tsx
@@ -21,7 +21,7 @@ export default function Article({
   return (
     <div className="w-3/4 pb-16 flex flex-col items-center h-[calc(100vh-64px)] overflow-y-scroll overflow-x-hidden px-8" ref={containerRef}>
       <h1 className="font-semibold text-3xl mt-6">{articleName}</h1>
-      <h2 className="font-normal text-xl mt-2">By: {authors}</h2>
+      <h2 className="font-normal text-xl mt-2 mb-12">By: {authors}</h2>
       <ReactMarkdown>{body}</ReactMarkdown>
       <motion.div className="fixed bottom-0 left-[25vw] bg-gradient-to-r from-cyan-500 to-blue-500 origin-[0%] h-1 w-3/4 z-50" style={{scaleX: scrollYProgress}}></motion.div>
     </div>
--- app/components/Video.tsx
+++ app/components/Video.tsx
@@ -9,8 +9,8 @@ export default function Video({
 }) {
   return (
     <div className="w-3/4 h-[calc(100vh-64px)] overflow-y-scroll overflow-x-hidden px-8 flex flex-col items-center">
-      <h1 className="font-semibold text-3xl my-6">{title}</h1>
-      <iframe width="1000px" height="500px" src={link}></iframe>
+      <h1 className="font-semibold text-3xl my-6 mb-16">{title}</h1>
+      <iframe width="1000px" height="500px" src={link} className="rounded-xl shadow-xl"></iframe>
     </div>
   );
 }
