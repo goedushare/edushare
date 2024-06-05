f5283e4401afc71f5e7aaaa1a764927f44af9af1
elianabean <ewanginspiration@gmail.com>
2024-06-05 16:35:48 -0400

    sidebar animations

--- app/components/Video.tsx
+++ app/components/Video.tsx
@@ -9,8 +9,8 @@ export default function Video({
 }) {
   return (
     <div className="w-3/4 h-[calc(100vh-64px)] overflow-y-scroll overflow-x-hidden px-8 flex flex-col items-center">
-      <h1 className="font-semibold text-3xl my-6 mb-16">{title}</h1>
-      <iframe width="1000px" height="500px" src={link} className="rounded-xl shadow-xl"></iframe>
+      <h1 className="font-semibold text-3xl my-6 mb-10">{title}</h1>
+      <iframe width="1000px" height="600px" src={link} className="rounded-xl shadow-xl mb-16"></iframe>
     </div>
   );
 }
