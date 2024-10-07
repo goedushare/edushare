7c26ccd1ad48022433ee5d5b863cfc2f77933a91
zeyuanzhao <14340342+zeyuanzhao@users.noreply.github.com>
2024-10-07 12:00:10 -0400

    dashboard and new pages

---
+++ app/(dashboard)/new/article/page.tsx
@@ -0,0 +1,9 @@
+const NewArticle = () => {
+  return (
+    <div>
+      <h1>New Article</h1>
+    </div>
+  );
+};
+
+export default NewArticle;
---
+++ app/(dashboard)/new/quiz/page.tsx
@@ -0,0 +1,9 @@
+const NewQuiz = () => {
+  return (
+    <div>
+      <h1>New Quiz</h1>
+    </div>
+  );
+};
+
+export default NewQuiz;
