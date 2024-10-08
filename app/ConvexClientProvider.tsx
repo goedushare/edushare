995f05ee2bb987eb7cab616e173824c4736587e3
zeyuanzhao <14340342+zeyuanzhao@users.noreply.github.com>
2024-10-07 22:13:52 -0400

    disable convex

--- app/ConvexClientProvider.tsx
+++ app/ConvexClientProvider.tsx
@@ -1,21 +1,21 @@
-"use client";
-import { ReactNode } from "react";
-import { ConvexProvider, ConvexReactClient } from "convex/react";
-import { SessionProvider } from "@convex-dev/convex-lucia-auth/react";
-import { AfterSSR } from "./components/helpers/AfterSSR";
+// "use client";
+// import { ReactNode } from "react";
+// import { ConvexProvider, ConvexReactClient } from "convex/react";
+// import { SessionProvider } from "@convex-dev/convex-lucia-auth/react";
+// import { AfterSSR } from "./components/helpers/AfterSSR";
 
-const convex = new ConvexReactClient("https://tame-seahorse-621.convex.cloud");
+// const convex = new ConvexReactClient("https://tame-seahorse-621.convex.cloud");
 
-export default function ConvexClientProvider({
-  children,
-}: {
-  children: ReactNode;
-}) {
-  return (
-    <AfterSSR>
-      <ConvexProvider client={convex}>
-        <SessionProvider>{children}</SessionProvider>
-      </ConvexProvider>
-    </AfterSSR>
-  );
-}
+// export default function ConvexClientProvider({
+//   children,
+// }: {
+//   children: ReactNode;
+// }) {
+//   return (
+//     <AfterSSR>
+//       <ConvexProvider client={convex}>
+//         <SessionProvider>{children}</SessionProvider>
+//       </ConvexProvider>
+//     </AfterSSR>
+//   );
+// }
