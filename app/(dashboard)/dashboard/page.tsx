4de86d24377176760aa3ed4b183205c23eb63c94
zeyuanzhao <14340342+zeyuanzhao@users.noreply.github.com>
2024-10-11 13:00:16 -0400

    modal component

--- app/(dashboard)/dashboard/page.tsx
+++ app/(dashboard)/dashboard/page.tsx
@@ -1,13 +1,35 @@
-import { Button } from "@nextui-org/react";
+"use client";
+
+import { Button, useDisclosure } from "@nextui-org/react";
 import Module from "@/components/Module";
 import modules from "@/assets/modules.json";
+import ModuleModal from "@/components/Modal";
 
 const Dashboard = () => {
+  const {
+    isOpen: isModalOpen,
+    onOpen: onModalOpen,
+    onOpenChange: onModalOpenChange,
+  } = useDisclosure();
+
+  const onCreate = (onClose: () => void) => {
+    onClose();
+  };
+
   return (
     <div>
       <div className="flex flex-row justify-between">
         <h1 className="text-4xl">My Modules</h1>
-        <Button className="bg-[#0E793C] text-white">Create New</Button>
+        <Button className="bg-[#0E793C] text-white" onPress={onModalOpen}>
+          Create New
+        </Button>
+        <ModuleModal
+          isOpen={isModalOpen}
+          onOpenChange={onModalOpenChange}
+          title="Create Module"
+          actionText="Create"
+          onAction={onCreate}
+        ></ModuleModal>
       </div>
       <div className="mt-4 mb-8">
         {modules["modules"].map((module) => {
---
+++ components/Modal.tsx
@@ -0,0 +1,53 @@
+import {
+  Button,
+  Modal as NextModal,
+  ModalBody,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+} from "@nextui-org/react";
+import TextField from "./TextField";
+
+const ModalComponent = ({
+  isOpen,
+  onOpenChange,
+  title,
+  children,
+  actionText,
+  onAction,
+}: {
+  isOpen: boolean;
+  onOpenChange: () => void;
+  title: string;
+  children?: React.ReactNode;
+  actionText?: string;
+  onAction?: (onClose: () => void) => void;
+}) => {
+  return (
+    <NextModal isOpen={isOpen} onOpenChange={onOpenChange}>
+      <ModalContent>
+        {(onClose) => (
+          <>
+            <ModalHeader>{title}</ModalHeader>
+            <ModalBody>{children}</ModalBody>
+            <ModalFooter>
+              <Button className="" onPress={onClose}>
+                Cancel
+              </Button>
+              {onAction && actionText && (
+                <Button
+                  className="bg-[#0E793C] text-white"
+                  onPress={() => onAction(onClose)}
+                >
+                  {actionText}
+                </Button>
+              )}
+            </ModalFooter>
+          </>
+        )}
+      </ModalContent>
+    </NextModal>
+  );
+};
+
+export default ModalComponent;
