28ec195b27a1eeb9825ca9ed9bf7ab557f8d5818
Arjun99ab <53882059+Arjun99ab@users.noreply.github.com>
2024-10-20 23:33:23 -0400

    render login & register errors

--- app/(auth)/register/page.tsx
+++ app/(auth)/register/page.tsx
@@ -6,22 +6,39 @@ import Link from "next/link";
 import { useState } from "react";
 import { auth } from '@/lib/firebaseConfig';
 import { createUserWithEmailAndPassword } from 'firebase/auth';
+import { useRouter } from 'next/navigation';
+
 
 const Register = () => {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [error, setError] = useState(null);
+  const [error, setError] = useState("");
   const [success, setSuccess] = useState(false);
 
+  const router = useRouter();
+
   const handleRegister = async () => {
+    setError("");
     try {
       const userCredential = await createUserWithEmailAndPassword(auth, email, password);
       setSuccess(true);
       console.log('User registered:', userCredential.user);
+      router.push('/login');
+
     } catch (error) {
-      // setError(error.message);
-      console.error('Error registering:', error);
+      // Handle different Firebase auth errors
+      const firebaseError = error as { code: string };
+      if (firebaseError.code === "auth/weak-password") {
+        setError("Password is too short. It should be at least 6 characters.");
+      } else if (firebaseError.code === "auth/email-already-in-use") {
+        setError("This email is already in use.");
+      } else if (firebaseError.code === "auth/invalid-email") {
+        setError("The email address is not valid.");
+      } else {
+        setError("Failed to register. Please try again.");
+      }
+      // console.error('Error registering:', firebaseError);
     }
   };
 
@@ -53,6 +70,8 @@ const Register = () => {
             label="Password"
             className="basis-1/6"
           />
+
+          {error && <p className="text-red-500 text-sm mt-2">{error}</p>}
           <div className="basis-1/2 flex flex-col justify-end">
             <div className="flex flex-row justify-between items-end">
               <Button onClick={handleRegister} type="submit" className="bg-[#0E793C] text-white">
